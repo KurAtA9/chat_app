f158727a03e8f3e6b5254237666879e71b8235e2
KurAtA9 <kurata@ibis-japan.co.jp>
2026-05-08 13:04:02 +0900

    プルダウンの実装

--- src/main/java/in/tech_camp/chat_app/repository/UserRepository.java
+++ src/main/java/in/tech_camp/chat_app/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package in.tech_camp.chat_app.repository;
 
+import java.util.List;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
@@ -28,4 +29,7 @@ public interface  UserRepository {
 
   @Select("SELECT COUNT(*) > 0 FROM users WHERE email = #{email} AND userId != #{userId}")
   boolean existsByEmailExcludingCurrent(String email, Integer id);
+
+  @Select("SELECT * FROM users WHERE id <> #{excludedId}")
+  List<UserEntity> findAllExcept(Integer excludedId);
 }

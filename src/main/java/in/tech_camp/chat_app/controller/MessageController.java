72e290bc579834883a31a451584a1550fa09c3e7
KurAtA9 <kurata@ibis-japan.co.jp>
2026-05-08 16:12:48 +0900

    チャット画面の編集

--- src/main/java/in/tech_camp/chat_app/controller/MessageController.java
+++ src/main/java/in/tech_camp/chat_app/controller/MessageController.java
@@ -1,16 +1,20 @@
 package in.tech_camp.chat_app.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import in.tech_camp.chat_app.custom_user.CustomUserDetail;
+import in.tech_camp.chat_app.entity.RoomEntity;
+import in.tech_camp.chat_app.entity.RoomUserEntity;
 import in.tech_camp.chat_app.entity.UserEntity;
+import in.tech_camp.chat_app.repository.RoomUserRepository;
 import in.tech_camp.chat_app.repository.UserRepository;
+import lombok.AllArgsConstructor;
 
 
 
@@ -18,11 +22,19 @@ import in.tech_camp.chat_app.repository.UserRepository;
 @AllArgsConstructor
 public class MessageController {
   private final UserRepository userRepository;
-
+  private final RoomUserRepository roomUserRepository;
+  
   @GetMapping("/")
   public String showMessages(@AuthenticationPrincipal CustomUserDetail currentUser, Model model) {
     UserEntity user = userRepository.findById(currentUser.getId());
     model.addAttribute("user", user);
+    
+    List<RoomUserEntity> roomUserEntities = roomUserRepository.findByUserId(currentUser.getId());
+    List<RoomEntity> roomList = roomUserEntities.stream()
+        .map(RoomUserEntity::getRoom)
+        .collect(Collectors.toList());
+    model.addAttribute("rooms", roomList);
     return "messages/index";
-  }
+
+   }
 }

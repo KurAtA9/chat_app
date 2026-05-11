3a3d5f509865aabe34161f2519ee505df4442311
KurAtA9 <kurata@ibis-japan.co.jp>
2026-05-11 16:40:43 +0900

    画像保存の実装

--- src/main/java/in/tech_camp/chat_app/controller/MessageController.java
+++ src/main/java/in/tech_camp/chat_app/controller/MessageController.java
@@ -1,5 +1,11 @@
 package in.tech_camp.chat_app.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,7 +17,10 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
+import in.tech_camp.chat_app.ImageUrl;
 import in.tech_camp.chat_app.custom_user.CustomUserDetail;
 import in.tech_camp.chat_app.entity.MessageEntity;
 import in.tech_camp.chat_app.entity.RoomEntity;
@@ -24,8 +33,6 @@ import in.tech_camp.chat_app.repository.RoomUserRepository;
 import in.tech_camp.chat_app.repository.UserRepository;
 import in.tech_camp.chat_app.validation.ValidationOrder;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -38,6 +45,8 @@ public class MessageController {
   private final RoomUserRepository roomUserRepository;
   private final RoomRepository roomRepository;
   private final MessageRepository messageRepository;
+  private final ImageUrl imageUrl;
+
 
 
   @GetMapping("rooms/{roomId}/messages")
@@ -62,13 +71,30 @@ public class MessageController {
   @PostMapping("/rooms/{roomId}/messages")
   public String saveMessage(@PathVariable("roomId") Integer roomId, @ModelAttribute("messageForm") @Validated(ValidationOrder.class) MessageForm messageForm, BindingResult bindingResult, @AuthenticationPrincipal CustomUserDetail currentUser) {
     
-    // DBにセットする情報：content,user_id,room_id,image（残り2つは勝手にやってくれる）→ @pathvariableでroomId, @AuthenticationPrincipalでログイン中のuser_idを、
+    // DBにセットする情報：content,user_id,room_id,image（残り2つは勝手にやってくれる）→ @pathvariableでroomId, @AuthenticationPrincipalでログイン中のuser_idを、MultipartFileでimageを、form入力情報からcontentを
     if(bindingResult.hasErrors()){
       return "redirect:/rooms/" + roomId + "/messages";
     }
 
     MessageEntity message = new MessageEntity();
     message.setContent(messageForm.getContent());
+
+    MultipartFile imageFile = messageForm.getImage();
+    if (imageFile != null && !imageFile.isEmpty()) {
+      try {
+        String uploadDir = imageUrl.getImageUrl();
+        String fileName = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")) + "_" + imageFile.getOriginalFilename();
+        //fileName[String型]に、｛【yyyymmddHHmmssの型に調整したローカルデバイスのその時の日時】_【アップするファイルのオリジナルの名前】｝で格納
+        Path imagePath = Paths.get(uploadDir, fileName);
+        //uploadDir(=src/main/resources/static/upload)とfilename(=yyyymmddHHmmss_[ファイル名])をくっつけてパス調整しときますね～
+        Files.copy(imageFile.getInputStream(), imagePath);
+        //さっき作った保存場所(imagePass)にimageFileをコピーします！ の書き方
+        message.setImage("/uploads/" + fileName);
+      } catch (IOException e) { //Files.copy 使う際はこれを記述しておくべき
+        e.printStackTrace();
+        return "redirect:/rooms/" + roomId + "/messages";
+      }
+    }
 
     UserEntity user = userRepository.findById(currentUser.getId());
     RoomEntity room = roomRepository.findById(roomId);
@@ -79,7 +105,7 @@ public class MessageController {
       messageRepository.insert(message);
     } catch (Exception e) {
       System.out.println("エラー：" + e);
-   }
+    }
 
    return String.format("redirect:/rooms/%d/messages", roomId);
   }
